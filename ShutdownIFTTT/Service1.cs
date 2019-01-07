b2ef9268039fd1876d17be84d317ae7ca9dda75d
Desmet <desmetma@delawareconsulting.com>
2019-01-07 11:56:52 +0100

    Add project files.

---
+++ ShutdownIFTTT/Service1.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.ServiceProcess;
+using System.Timers;
+
+namespace ShutdownIFTTT
+{
+    public partial class Service1 : ServiceBase
+    {
+        private string filePath = "C:\\Users\\desmetma\\Documents\\junk\\test.txt";
+        private Timer timer = new Timer();
+
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            System.Threading.Thread.Sleep(60000);
+
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+            }
+            timer.Elapsed += new ElapsedEventHandler(OnElapsedTime);
+            timer.Interval = 5000;  
+            timer.Enabled = true;
+        }
+
+        private void OnElapsedTime(object source, ElapsedEventArgs e)
+        {
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+                var psi = new ProcessStartInfo("shutdown", "/s /t 0")
+                {
+                    CreateNoWindow = true,
+                    UseShellExecute = false
+                };
+                Process.Start(psi);
+            }
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
